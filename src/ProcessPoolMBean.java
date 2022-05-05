44d6401381271db88692eaeb0b9ec278380e5152
Sokolov <karkarych08@gmail.com>
2022-05-05 17:28:36 +0300

    create different folders

--- src/ProcessPoolMBean.java
+++
@@ -1,7 +0,0 @@
-public interface ProcessPoolMBean {
-    void submit(String name, String classpath, String mainClass, int period) throws ClassNotFoundException;
-
-    String status(String name);
-
-    void cancel(String name);
-}
